5edc83c747cdf16d63b08b479b18383a8c56fb9c
Jordan Falcon <jfalcon9319@gmail.com>
2023-05-04 00:08:44 -0500

    updated header date & added total to load container

--- components/header.js
+++ components/header.js
@@ -6,7 +6,7 @@ export default function Header({ children }) {
       <div className="">
         <h2>jordan falcon</h2>
         <h2>software engineer | fargo, nd</h2>
-        <h2 className="text-gray-400">updated 04-30-2023</h2>
+        <h2 className="text-gray-400">updated 05-04-2023</h2>
       </div>
     </>
   );
