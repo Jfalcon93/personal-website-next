34a4f182b5029feaae464cada58b20790cca81ff
Jordan Falcon <jfalcon9319@gmail.com>
2023-05-17 14:39:01 -0500

    Updated header dates and icon hover colors

--- components/header.js
+++ components/header.js
@@ -5,8 +5,8 @@ export default function Header({ children }) {
     <>
       <div className="">
         <h2>jordan falcon</h2>
-        <h2>software engineer | fargo, nd</h2>
-        <h2 className="text-gray-400">updated 05-04-2023</h2>
+        <h2>senior software engineer | fargo, nd</h2>
+        <h2 className="text-gray-400">updated 05-17-2023</h2>
       </div>
     </>
   );
