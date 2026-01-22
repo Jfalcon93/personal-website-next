c358b4516b2a0a2b463329d850f32122d1855dd4
Jordan Falcon <jfalcon9319@gmail.com>
2026-01-22 16:01:38 -0600

    updating styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,6 @@
-module.exports = {
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      fontSize: {
-        "10xl": ["9rem", "1"],
-      },
-      transitionProperty: {
-        width: "width",
-        position: "position",
-      },
-      visibility: ["group-hover"],
-    },
-  },
-  plugins: [],
 };
